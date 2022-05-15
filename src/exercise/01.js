c4a851f68c16d393e524415d36e8bb09eb56b578
coofzilla <jeric614@yahoo.com>
2022-05-15 17:06:36 +0900

    refactor using traditional reducer w/switch case

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,18 +3,26 @@
 
 import * as React from 'react'
 
-const countReducer = (state, action) => ({
-  ...state,
-  ...(typeof action === 'function' ? action(state) : action),
-})
+const countReducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT': {
+      return {
+        ...state,
+        count: state.count + action.step,
+      }
+    }
+    default: {
+      throw new Error(`Unsupported action type: ${action.type}`)
+    }
+  }
+}
 
 function Counter({initialCount = 0, step = 1}) {
-  const [state, setState] = React.useReducer(countReducer, {
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
-  const increment = () =>
-    setState(currentState => ({count: currentState.count + step}))
+  const increment = () => dispatch({type: 'INCREMENT', step})
 
   return <button onClick={increment}>{count}</button>
 }
