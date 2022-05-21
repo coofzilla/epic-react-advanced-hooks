5e832c0c393782dfd14b363b25957b7e798bd70e
coofzilla <jeric614@yahoo.com>
2022-05-21 11:39:14 +0900

    exercise 3 passed w/cache

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -24,6 +24,7 @@ function CountDisplay() {
 
 function Counter() {
   const [, setCount] = useCount()
+
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
